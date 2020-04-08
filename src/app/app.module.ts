6c1d5a019e684099d8bcd66b64ca757938a96195
Deo-Mac <druplall@gmail.com>
2020-04-08 11:41:53 -0400

    Completed Starting Phase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { EventsAppComponent } from './events-app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  // components, pipes and other must be in the declarations array
+  declarations: [EventsAppComponent],
+  // Importing modules from outside libraries
+  imports: [BrowserModule],
+  // Services will be in the provider
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [EventsAppComponent],
 })
-export class AppModule { }
+export class AppModule {}
