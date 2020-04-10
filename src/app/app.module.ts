a7986d0624a2a1746bdfeb2ea66023201e856410
Deo-Mac <druplall@gmail.com>
2020-04-10 09:26:23 -0400

    completed feature branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './events-app.component';
+import { EventListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/events-thumbnail.components';
+import { NavBarComponent } from './nav/navbar.component';
 
 @NgModule({
   // components, pipes and other must be in the declarations array
-  declarations: [EventsAppComponent],
+  declarations: [
+    EventsAppComponent,
+    EventListComponent,
+    EventThumbnailComponent,
+    NavBarComponent,
+  ],
   // Importing modules from outside libraries
   imports: [BrowserModule],
   // Services will be in the provider
